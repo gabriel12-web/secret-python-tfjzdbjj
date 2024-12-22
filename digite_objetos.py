4ccf5eb7fd3be2568633bd65dc52bedf8912a03c
gabriel1208 <gabrielbarretodasilva120@gmail.com>
2024-12-22 13:09:55 -0300

    Add files via upload

---
+++ digite_objetos.py
@@ -0,0 +1,4 @@
+objeto=input('Digite algo que tenha no seu quarto:')
+objeto2=input('Digite algo que tenha no quarto do seus pais:')
+objeto3=input('Digite algo que tenha no seu banheiro:')
+print('No se quarto te um(a) {:.>},No quarto dos seus pais tem um(a) {} e no seu banheiro tem um(a) {}'.format(objeto,objeto2,objeto3))
